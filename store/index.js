b4d12c425b4763f89223a87054bbad798a7bfd99
miknieyh <tong_hye@naver.com>
2022-03-29 17:58:27 +0900

    코드정리

--- store/index.js
+++ store/index.js
@@ -10,6 +10,7 @@ const store = () => new Vuex.Store({
     currentYear: new Date().getFullYear(),
     firstMonth: 0,
     lastMonth: 11,
+    weekDay: 6,
     clickDateString: "",
     selectDates: [],
     drawCalEx: [],
@@ -20,7 +21,7 @@ const store = () => new Vuex.Store({
     YEAR_NAME: "년",
     MONTH_NAME: "월",
     DATE_NAME: "일",
-    hyphen : '-'
+    hyphen: '-'
   },
   mutations: {
     firstDay(state) {
@@ -31,22 +32,31 @@ const store = () => new Vuex.Store({
       }
     },
     updateClickDate(state, newClickDate) {
-      state.clickDateString = state.currentYear.toString()+state.YEAR_NAME+(state.currentMonthInNumber + 1).toString()+state.MONTH_NAME+newClickDate.date["date"]+state.DATE_NAME;
-      state.clickDate = state.currentYear.toString() + state.hyphen + (state.currentMonthInNumber + 1).toString() + state.hyphen + newClickDate.date["date"];
+      state.clickDateString =
+        state.currentYear.toString()
+        + state.YEAR_NAME
+        + (state.currentMonthInNumber + 1).toString()
+        + state.MONTH_NAME + newClickDate.date["date"] + state.DATE_NAME;
+      state.clickDate =
+        state.currentYear.toString()
+        + state.hyphen
+        + (state.currentMonthInNumber + 1).toString()
+        + state.hyphen
+        + newClickDate.date["date"];
     },
     prev(state) {
-      const isFirstMonth = () =>state.currentMonthInNumber === state.firstMonth;
+      const isFirstMonth = () => state.currentMonthInNumber === state.firstMonth;
       if (isFirstMonth()) { //0
         state.currentYear--;
         state.currentMonthInNumber = state.lastMonth; //11
       }
 
-      if(!isFirstMonth()) {
+      if (!isFirstMonth()) {
         state.currentMonthInNumber--;
       }
     },
     next(state) {
-      const isLastMonth = () =>state.currentMonthInNumber === state.lastMonth;
+      const isLastMonth = () => state.currentMonthInNumber === state.lastMonth;
       if (isLastMonth()) { //11
         state.currentYear++;
         state.currentMonthInNumber = state.firstMonth; //0
@@ -68,7 +78,13 @@ const store = () => new Vuex.Store({
       let curDate = new Date(document.getElementById('startDate').value);
       const endDate = new Date(document.getElementById('endDate').value);
 
-      const isCurDate = (date) => (curDate.getFullYear().toString()+state.hyphen+(curDate.getMonth() + 1).toString()+state.hyphen+curDate.getDate().toString()) === date;
+      const isCurDate =
+        (date) => (curDate.getFullYear().toString()
+          + state.hyphen
+          + (curDate.getMonth() + 1).toString()
+          + state.hyphen
+          + curDate.getDate().toString()) === date;
+
       while (curDate <= endDate) {
         let isNational = false;
         let temp = {};
@@ -88,43 +104,51 @@ const store = () => new Vuex.Store({
         for (let i in state.nationalDayList[0]) {
           let tempDate = String(state.nationalDayList[0][i]["locdate"]);
           const NationalDate = new Date(tempDate.substring(0, 4), tempDate.slice(4, 6), tempDate.slice(6));
-          isNational = isCurDate(this.setDateFormat(NationalDate.getFullYear().toString(), NationalDate.getMonth().toString(), NationalDate.getDate().toString()))
+          isNational =
+            isCurDate((NationalDate.getFullYear().toString()
+              + state.hyphen
+              + NationalDate.getMonth().toString() + state.hyphen
+              + NationalDate.getDate().toString()))
 
           temp.isNationalDay = isNational ? "예" : "아니오";
-
         }
         state.selectDates.push(temp);
         curDate.setDate(curDate.getDate() + 1);
       }
     },
-    drawCalendar(state) {
+    drawCalendar(state, getters) {
       state.drawCal = [];
       state.drawCalEx = [];
       state.drawCalAfter = [];
       let firstDayList = state.firstDayList;
-      const lastDayOfLastMonth = new Date(state.currentYear, state.currentMonthInNumber, 0).getDate();
-      const lastDayOfCurrentMonth = new Date(state.currentYear, state.currentMonthInNumber + 1, 0).getDate();
 
       //전달 날짜 표시
       for (let date in firstDayList) {
-        state.drawCalEx.push(lastDayOfLastMonth - firstDayList[date] + 1);
+        state.drawCalEx.push(getters.lastDayOfLastMonth - firstDayList[date] + 1);
       }
       //다음달 날짜 표시
-      const afterDate = 6 - new Date(state.currentYear, state.currentMonthInNumber, lastDayOfCurrentMonth).getDay();
+
+      const afterDate = state.weekDay - getters.currentDay;
       for (let date = 1; date <= afterDate; date++) {
         state.drawCalAfter.push(date);
       }
       //이달 날짜 표시
       const selectDatesNotNull = selectDates => selectDates !== null && selectDates !== [];
 
-      for (let date = 1; date < lastDayOfCurrentMonth + 1; date++) {
+      for (let date = 1; date < getters.lastDayOfCurrentMonth + 1; date++) {
 
         if (selectDatesNotNull(state.selectDates)) {
-          const selectDatesList = state.selectDates.map(row=>row.date);
-          let isSelect = selectDatesList.includes((state.currentYear.toString()+state.hyphen+(state.currentMonthInNumber+1).toString()+state.hyphen+date.toString())).toString();
+          const selectDatesList = state.selectDates.map(row => row.date);
+          let isSelect =
+            selectDatesList.includes(
+              (state.currentYear.toString()
+                + state.hyphen
+                + (state.currentMonthInNumber + 1).toString()
+                + state.hyphen + date.toString())).toString();
+
           let day = new Date(state.currentYear, state.currentMonthInNumber, date).getDay()
 
-          state.drawCal.push({"date": date.toString(),"select": isSelect, "day": day});
+          state.drawCal.push({"date": date.toString(), "select": isSelect, "day": day});
         }
       }
     }
@@ -143,8 +167,8 @@ const store = () => new Vuex.Store({
       context.commit('lookUpPage');
     },
     getNationalList({commit}, currentYear) {
-      let url = "http://localhost:3000/api";
-      let ServiceKey = '?_type=json&ServiceKey=pttHWIl4dfMuWu4ZBaBagNtAzamjrs%2BMGE9JDETUED7tu4y1Dt8ajPP7qmxXBJZQTLLhFHjZ84EkuMHfkxZcnA%3D%3D&solYear=';
+      const url = "http://localhost:3000/api";
+      const ServiceKey = '?_type=json&ServiceKey=pttHWIl4dfMuWu4ZBaBagNtAzamjrs%2BMGE9JDETUED7tu4y1Dt8ajPP7qmxXBJZQTLLhFHjZ84EkuMHfkxZcnA%3D%3D&solYear=';
       console.log('test');
       axios.get(url + ServiceKey + currentYear.toString(), {headers: {"Accept": "application/json"}})
         .then(res => {
@@ -172,6 +196,15 @@ const store = () => new Vuex.Store({
     rows: (state) => {
       return state.selectDates.length;
     },
+    currentDay: (state) => {
+      return new Date(state.currentYear, state.currentMonthInNumber, this.lastDayOfCurrentMonth()).getDay();
+    },
+    lastDayOfCurrentMonth: (state) => {
+      return new Date(state.currentYear, state.currentMonthInNumber + 1, 0).getDate();
+    },
+    lastDayOfLastMonth: (state) => {
+      return new Date(state.currentYear, state.currentMonthInNumber, 0).getDate();
+    }
   }
 })
 
